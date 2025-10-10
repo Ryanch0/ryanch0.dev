fc72a2af5810f835cb7baadc91dc39e0551ee24c
ryanch0 <tjddnfkdls0@naver.com>
2025-10-10 14:24:51 +0900

    feat: setup msw

---
+++ src/msw/MswWrapper/MswWrapper.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { PropsWithChildren, useEffect, useState } from 'react'
+
+import { setupWorker } from 'msw/browser'
+
+const worker = setupWorker() //TODO add mock api
+
+export default function MSWWrapper({ children }: PropsWithChildren) {
+  const [ready, setReady] = useState(false)
+
+  useEffect(() => {
+    if (process.env.NEXT_PUBLIC_API_MOCKING !== 'true') {
+      // eslint-disable-next-line react-hooks/set-state-in-effect
+      setReady(true)
+
+      return
+    }
+    worker.start({ onUnhandledRequest: 'bypass' }).then(() => setReady(true))
+  }, [])
+
+  if (!ready) return null
+
+  return <>{children}</>
+}
