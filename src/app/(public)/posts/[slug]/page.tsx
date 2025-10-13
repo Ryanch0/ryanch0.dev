6d6391f5a0ddb5aea4890fe47a6c92cac98e1ec7
ryanch0 <tjddnfkdls0@naver.com>
2025-10-13 14:47:18 +0900

    feat: add post,tag components & scaffold to posts page

--- src/app/(public)/posts/[slug]/page.tsx
+++ src/app/(public)/posts/[slug]/page.tsx
@@ -1,5 +1,31 @@
-const Page = () => {
-  return <>POST_DETAIL</>
+import { MOCK_POST_DETAIL } from '@/constants/mock'
+import PostDetail from '@/features/posts/components/PostDetail'
+import Hero from '@/shared/components/Hero'
+import { NextParsedUrlQuery } from 'next/dist/server/request-meta'
+import Link from 'next/link'
+
+type Props = {
+  params: NextParsedUrlQuery
+}
+const Page = ({ params }: Props) => {
+  const { slug } = params
+  const { id, title, date, last_modified } = MOCK_POST_DETAIL //TODO from db
+
+  return (
+    <>
+      <Hero>
+        <p>{slug}</p>
+        <h1>{title}</h1>
+        <p>Published {date}</p>
+        <p>Last updated {last_modified}</p>
+        <Link href={`/write/${id}`}>Update</Link>
+        <form action="">
+          <button type="submit">Delete</button>
+        </form>
+      </Hero>
+      <PostDetail />
+    </>
+  )
 }
 
 export default Page
--- src/app/(public)/posts/page.tsx
+++ src/app/(public)/posts/page.tsx
@@ -1,5 +1,25 @@
+import { Suspense } from 'react'
+
+import PostList from '@/features/posts/components/PostList'
+import TagList from '@/features/tags/components/TagList'
+import Hero from '@/shared/components/Hero'
+
 const Page = () => {
-  return <>POSTS_LIST</>
+  return (
+    <>
+      <Hero>
+        <p className={'text-black'}>HERO SECTION</p>
+      </Hero>
+      <main className={'flex justify-between'}>
+        <Suspense fallback={'fetching post list...'}>
+          <PostList />
+        </Suspense>
+        <Suspense fallback={'fetching tags list...'}>
+          <TagList />
+        </Suspense>
+      </main>
+    </>
+  )
 }
 
 export default Page
