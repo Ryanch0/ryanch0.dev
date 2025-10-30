c5d74e0e310de83c2a932c9af1e6da59bc97c568
ryanch0 <tjddnfkdls0@naver.com>
2025-10-30 14:27:20 +0900

    refactor: Add extensible props

--- src/shared/components/MainLink.tsx
+++ src/shared/components/MainLink.tsx
@@ -1,16 +1,30 @@
+import { ReactNode } from 'react'
+
 import { PATH } from '@/constants/path'
 import Link from 'next/link'
 
 type Props = {
   className?: string
+  href?: string
+  title?: ReactNode
+  target?: string
+  rel?: string
 }
-const MainLink = ({ className }: Props) => {
+const MainLink = ({
+  className,
+  href = PATH.MAIN,
+  title = 'Ryan Cho',
+  target,
+  rel
+}: Props) => {
   return (
     <Link
       className={`text-accent-light dark:text-accent-dark decoration-underline-light dark:decoration-underline-dark hover:bg-section-light dark:hover:bg-section-dark inline-block rounded-sm px-1 underline decoration-1 underline-offset-4 hover:no-underline ${className}`}
-      href={PATH.MAIN}
+      href={href}
+      rel={rel}
+      target={target}
     >
-      Ryan Cho
+      {title}
     </Link>
   )
 }
