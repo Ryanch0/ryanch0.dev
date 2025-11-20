4c02a582b96e5ceb467b9be9f26875aa9ef6302b
ryanch0 <tjddnfkdls0@naver.com>
2025-11-20 17:14:18 +0900

    feat: Add error.tsx in locale layout

---
+++ src/app/[lang]/error.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import Footer from '@/shared/components/Footer'
+import Header from '@/shared/components/Header'
+import MainLink from '@/shared/components/MainLink'
+
+const Error = () => {
+  return (
+    <main className="layout">
+      <Header />
+      <div className={'flex flex-col gap-30 pt-18'}>
+        <div>
+          <h2 className={'title-style'}>Error</h2>
+          <div className={'second-font-style py-1'}>
+            <p>Something went wrong</p>
+            <span>by</span> <MainLink />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </main>
+  )
+}
+
+export default Error
